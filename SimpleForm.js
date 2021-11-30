379aaaa3ddcb12d1947ed377a9eb14ccf89de16c
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-30 17:33:55 +0530

    Added: functioning of First Name

--- SimpleForm.js
+++ SimpleForm.js
@@ -5,5 +5,15 @@ salary.addEventListener('input', function () {
   output.textContent = salary.value;
 });
 
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function () {
+  let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+  if (nameRegex.test(text.value)) {
+    textError.textContent = "";
+  } else {
+    textError.textContent = "Name is Incorrect";
+  }
+});
 
 
